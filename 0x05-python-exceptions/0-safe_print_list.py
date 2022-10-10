3d4c6ed19390fff70c3c3b1cb950e32001928267
temitopeakin1 <temitopeakinmegha@gmail.com>
2022-10-10 11:19:00 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+
+
+def safe_print_list(my_list=[], x=0):
+    total = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            total += 1
+        except IndexError:
+            break
+    print("")
+    return (total)
