e3c9b1b7f169c7e0872845c9386fdc728e9bfee9
temitopeakin1 <temitopeakinmegha@gmail.com>
2022-12-01 18:47:44 -0800

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
-a-- Lists all comedy shows in the database hbtn_0d_tvshows.
+-- Lists all comedy shows in the database hbtn_0d_tvshows.
 -- Records are ordered by descending show title.
 SELECT t.`title`
   FROM `tv_shows` AS t
